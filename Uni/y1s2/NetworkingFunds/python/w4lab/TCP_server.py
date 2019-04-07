0d9c2d790f7fdcf2ccab059fdcb0a111d563e7eb
deBubz <bao.es.hoang2@gmail.com>
2019-04-07 18:10:10 +1000

    wtf w5 network

--- Uni/y1s2/NetworkingFunds/python/w4lab/TCP_server.py
+++ Uni/y1s2/NetworkingFunds/python/w4lab/TCP_server.py
@@ -4,7 +4,7 @@ print('lmao')
 sPort = 12001
 sSocket = socket(AF_INET, SOCK_STREAM)
 
-sSocket.bind(('172.19.147.184', sPort))
+sSocket.bind(('127.0.0.1', sPort))
 sSocket.listen(1)
 
 print('TCP is up Boye')
