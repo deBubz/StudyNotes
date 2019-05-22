d5b8e5a54523baff0a651f3bbed019865de0ab06
bubz <bao.es.hoang2@gmail.com>
2019-05-22 16:15:50 +1000

    comeon bud

---
+++ Uni/y1s2/DBFundamentals/a1/sqlPartD.sql
@@ -0,0 +1,74 @@
+/* Database Fundamentals, Assignment Part D, Autumn 2019) */
+/* Author:      Bao Hoang */
+/* Email:       Bao.T.Hoang@student.uts.edu.au */
+/* ScriptName:  insertName*/
+/* Purpose:     Basic PgSql scritp for deliveroo database containing 3 related tables*/
+/* Date:        insert date */
+/* Note:        Due to the limitation of only allowing 3 tables for the script,
+                The presented tables in this script are moddified to better
+                fit the requirements.*/
+
+-- DROP
+DROP TABLE tb1;
+DROP TABLE tb2;
+DROP TABLE tb3;
+
+-- 2.a CREATE / INSERT
+
+-- 2.a.1 tb1
+CREATE TABLE tb1();
+
+INSERT INTO tb1();
+
+-- 2.a.2 tb2
+CREATE TABLE tb2();
+
+INSERT INTO tb2();
+
+-- 2.a.3 tb3
+CREATE TABLE tb3();
+
+INSERT INTO tb3();
+
+-- 2.b.1 Question:
+-- SELECT * statement
+SELECT * FROM tb1;
+
+/*
+    table here
+*/
+
+-- 2.b.2 Question:
+-- SELECT * statement
+SELECT * FROM tb3;
+
+/*
+    table here
+*/
+
+-- 2.b.3 Question:
+-- SELECT * statement
+SELECT * FROM tb3;
+
+/*
+    table here
+*/
+
+-- 3.a Question: Select Statement with GroupBy
+-- The number of meals per in each order
+
+/*
+    table here
+*/
+
+-- 3.b Question: Inner Join
+
+/*
+    table here
+*/
+
+-- 3.c Question: Subquery
+
+/*
+    table here
+/
