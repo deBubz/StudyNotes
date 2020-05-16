c0b454978e74c2388a4228536807ec463a02ef44
Bao Hoang <Bao.T.Hoang@student.uts.edu.au>
2020-05-17 03:38:44 +1000

    lego

--- dsa/a1/working/working_map_list.hpp
+++ dsa/a1/working/working_map_list.hpp
@@ -44,32 +44,41 @@ public:
 	directed_graph();
 	~directed_graph();
 
+	// ok
 	bool contains(const int&) const;
 	bool adjacent(const int&, const int&);
 
+	// ok
 	void add_vertex(const vertex<T>&); //Adds the passed in vertex to the graph (with no edges).
 	void add_edge(const int&, const int&, const T&); //Adds a weighted edge from the first vertex to the second.
 
+	// ok
 	void remove_vertex(const int&); //Removes the given vertex. Should also clear any incident edges.
 	void remove_edge(const int&, const int&); //Removes the edge between the two vertices, if it exists.
 
+	// ok
 	size_t in_degree(const int&); //Returns number of edges coming in to a vertex.
 	size_t out_degree(const int&); //Returns the number of edges leaving a vertex.
 	size_t degree(const int&); //Returns the degree of the vertex (both in edges and out edges).
 
+	// ok
 	size_t num_vertices() const; //Returns the total number of vertices in the graph.
 	size_t num_edges() const; //Returns the total number of edges in the graph.
 
+	// ok
 	vector<vertex<T>> get_vertices(); //Returns a vector containing all the vertices.
 	vector<vertex<T>> get_neighbours(const int&); //Returns a vector containing all the vertices reachable from the given vertex. The vertex is not considered a neighbour of itself.
 	vector<vertex<T>> get_second_order_neighbours(const int&); // Returns a vector containing all the second_order_neighbours (i.e., neighbours of neighbours) of the given vertex.
 															  // A vector cannot be considered a second_order_neighbour of itself.
+	// todo
 	bool reachable(const int&, const int&) const; //Returns true if the second vertex is reachable from the first (can you follow a path of out-edges to get from the first to the second?). Returns false otherwise.
 	bool contain_cycles() const; // Return true if the graph contains cycles (there is a path from any vertices directly/indirectly to itself), false otherwise.
 
+	// todo
 	vector<vertex<T>> depth_first(const int&); //Returns the vertices of the graph in the order they are visited in by a depth-first traversal starting at the given vertex.
 	vector<vertex<T>> breadth_first(const int&); //Returns the vertices of the graph in the order they are visisted in by a breadth-first traversal starting at the given vertex.
 
+	// todo
 	directed_graph<T> out_tree(const int&); //Returns a tree starting at the given vertex using the out-edges. This means every vertex in the tree is reachable from the root.
 
 	vector<vertex<T>> pre_order_traversal(const int&, directed_graph<T>&); // returns the vertices in the visiting order of a pre-order traversal of the tree starting at the given vertex.
@@ -93,17 +102,12 @@ bool directed_graph<T>::contains(const int& u_id) const {
 	else return true;
 }
 
-template <typename T>	// check if 2 vert is linked/n
+template <typename T>	// check if 2 vert has an edge
 bool directed_graph<T>::adjacent(const int& u_id, const int& v_id) {
 	if (contains(u_id) && contains(v_id) && (u_id != v_id)) {
 		// if exist && no looped edges
-
-		unordered_map<int, T> found = this->adj_list[u_id];
-		for ( auto f : found ) {
-			if (f.first == v_id) return true;
-		}
-	}
-	return false;
+		return this->adj_list[u_id].find(v_id) != this->adj_list[u_id].end();
+	} else return false;
 }
 
 template <typename T>	// add vertex
@@ -119,12 +123,11 @@ template <typename T>	// add edge
 void directed_graph<T>::add_edge(const int& u_id, const int& v_id, const T& weight) {
 
 	if (contains(u_id) && contains(v_id)) {		// check existing vertexes
-		if (this->adj_list[u_id].find(v_id) == this->adj_list[u_id].end()) {	// check existing edges
+		if (!adjacent(u_id, v_id)) {	// check existing edges
 			this->adj_list[u_id].insert({v_id, weight});
 			this->edge_size++;
 		}
 	}
-
 }
 
 template <typename T>	// remove vertex
@@ -135,7 +138,7 @@ void directed_graph<T>::remove_vertex(const int& u_id) {
 		this->vertex_size--;
 
 		for (auto x : this->vertex_list) {	// delet v tail from list
-			adj_list[x.first].erase(u_id);
+			this->adj_list[x.first].erase(u_id);
 		}
 	}
 }
@@ -143,7 +146,7 @@ void directed_graph<T>::remove_vertex(const int& u_id) {
 template <typename T>	// remove edge (u,v)
 void directed_graph<T>::remove_edge(const int& u_id, const int& v_id) {
 	if(adjacent(u_id, v_id)) {
-
+		this->adj_list[u_id].erase(v_id);
 		this->edge_size--;
 	}
 }
@@ -152,7 +155,7 @@ template <typename T>
 size_t directed_graph<T>::in_degree(const int& u_id) {
 	size_t size = 0;
 	if(contains(u_id)) {							// check if exist
-		for (auto v : this->vertex_list) {	// foreach map(id, W) in list
+		for (auto v : this->vertex_list) {	// foreach map(id, W) in adj_list
 			// find edges v -> u
 			unordered_map<int, T> map = this->adj_list[v.first];
 			if(map.find(u_id) != map.end())
@@ -167,36 +170,34 @@ size_t directed_graph<T>::out_degree(const int& u_id) {
 	size_t size = 0;
 
 	if (contains(u_id)) {	// if exist
-
 		unordered_map<int, T> map = adj_list[u_id];
 		for (auto v: this->vertex_list ) {
 			if(map.find(v.first) != map.end()) size++;
 		}
 	}
-
 	return size;
+
+	// same as these but, ed's not happy
+	//return this->adj_list[u_id].size();
+	//return get_neighbours(u_id).size();
 }
 
 template <typename T>
 size_t directed_graph<T>::degree(const int& u_id) {
+	// would be a bit faster if implemented seperately
 	return in_degree(u_id) + out_degree(u_id);
 }
 
 template <typename T>	// return num of vertex
-size_t directed_graph<T>::num_vertices() const {
-	return this->vertex_list.size();
-
-}
+size_t directed_graph<T>::num_vertices() const { return this->vertex_size; }
 
 template <typename T>	// return num of edges
-size_t directed_graph<T>::num_edges() const {
-	size_t size = 0;
-	for (auto v : adj_list) {
-		size += v.second.size();
-	}
+size_t directed_graph<T>::num_edges() const { return this->edge_size; }
+	//size_t size = 0;
+	//for (auto v : adj_list) {
+	//	size += v.second.size();
+	//}
 
-	return size;
-}
 
 template <typename T>	// list of vertex
 vector<vertex<T>> directed_graph<T>::get_vertices() {
@@ -210,7 +211,7 @@ vector<vertex<T>> directed_graph<T>::get_vertices() {
 	return output_list;
 }
 
-template <typename T>
+template <typename T>	// first neighbours
 vector<vertex<T>> directed_graph<T>::get_neighbours(const int& u_id) {
 	vector<vertex<T>> v;
 	if (contains(u_id)) {
@@ -222,18 +223,16 @@ vector<vertex<T>> directed_graph<T>::get_neighbours(const int& u_id) {
 	return v;
 }
 
-template <typename T>
+template <typename T>	// second neighbours
 vector<vertex<T>> directed_graph<T>::get_second_order_neighbours(const int& u_id) {
-	vector<vertex<T>> firstN = get_neighbours(u_id);
-	vector<vertex<T>> secN, outN;
+	vector<vertex<T>> outN, secN,
+		firstN = get_neighbours(u_id);
 	bool flag[num_vertices() + 1];
 
 	// setting up flags for added verts
 	for (int i = 1; i < num_vertices() + 1; i++) {
 		flag[i] = false;
 	}
-
-	//cout << endl;
 
 	for (vertex<T> v1 : firstN) {						// foreach neighbour
 		secN = get_neighbours(v1.id);
@@ -245,19 +244,26 @@ vector<vertex<T>> directed_graph<T>::get_second_order_neighbours(const int& u_id
 		}
 	}
 
-	//for (vertex<T> v3 : outN) {
-	//	//cout << v3.id << " ";
-	//}
-	//cout << endl;
-
 	return outN;
 }
 
-template <typename T>
-bool directed_graph<T>::reachable(const int& u_id, const int& v_id) const { return false; }
+template <typename T>	// true if u can reach v
+bool directed_graph<T>::reachable(const int& u_id, const int& v_id) const {
+	if (adjacent(u_id, v_id)) return true;
 
-template <typename T>
-bool directed_graph<T>::contain_cycles() const { return false; }
+
+	return false;
+}
+
+template <typename T>	// true if graph have any cycles
+bool directed_graph<T>::contain_cycles() {
+	// should be the same as reachable
+	// check each v if it can reach itself
+
+	for(auto v : this->vertex_list) {
+		return reachable(v.first, v.first);
+	}
+}
 
 template <typename T>
 vector<vertex<T>> directed_graph<T>::depth_first(const int& u_id) { return vector<vertex<T>>(); }
